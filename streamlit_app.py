c8475fb59574837752c7abb5d5bed3ab85efab35
Madofuller <madofuller@gmail.com>
2022-09-16 18:25:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ from PIL import Image
 
 # Open images and store them in a list
 images = [Image.open(x) for x in ['canvas3.png', 'christmas card.jpg']]
-total_width = 3840
-max_height = 2160
+total_width = 0
+max_height = 0
 
 
 # find the width and height of the final image
