7b1cf4b46fda22af333ef0aa43af42ebee27733e
Madofuller <madofuller@gmail.com>
2022-09-16 18:29:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ im2 = Image.open('christmas card.jpg')
 im1 = Image.open('canvas3.png')
 
 back_im = im1.copy()
-back_im.paste(im2, (100, 50))
+back_im.paste(im2, (100, 100))
 back_im.save('frame-test.jpg', quality=95)
 
 st.image(back_im)
