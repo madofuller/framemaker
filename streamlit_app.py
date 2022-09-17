b814b7aa8b1b1a21a3b62497d77177425aec7337
Madofuller <madofuller@gmail.com>
2022-09-16 18:27:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,24 +2,9 @@ import streamlit as st
 from PIL import Image
 
 
+im1 = Image.open('christmas card.jpg')
+im2 = Image.open('canvas3.png')
 
-# Open images and store them in a list
-images = [Image.open(x) for x in ['canvas3.png', 'christmas card.jpg']]
-total_width = 0
-max_height = 0
-
-
-# find the width and height of the final image
-for img in images:
-    total_width += img.size[0]
-    max_height = max(max_height, img.size[1])
-# create a new image with the appropriate height and width
-new_img = Image.new('RGB', (total_width, max_height))
-# Write the contents of the new image
-current_width = 0
-for img in images:
-  new_img.paste(img, (current_width,0))
-  current_width += img.size[0]
-# Save the image
-new_img.save('NewImage.jpg')
-st.image(new_img)
+back_im = im1.copy()
+back_im.paste(im2, (100, 50))
+back_im.save('frame-test.jpg', quality=95)
